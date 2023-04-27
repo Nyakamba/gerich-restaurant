d48768844a3e92571220ba532140d5b68786135a
Nyakamba <omwegaenock@gmail.com>
2023-04-27 14:14:40 +0300

    header.jsx

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -20,7 +20,7 @@ const Header = () => (
     </div>
 
     <div className="app__wrapper_img">
-      <img src={images.welcome} alt="header img" />
+      <img src={images.welcome} alt="header_img" />
     </div>
   </div>
 );
