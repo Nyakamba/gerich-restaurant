5cfcf2e99a4042ade82d162b9f4ea3bd59a3e617
Nyakamba <omwegaenock@gmail.com>
2023-04-30 23:17:55 +0300

    Chef.jsx

--- src/container/Chef/Chef.jsx
+++ src/container/Chef/Chef.jsx
@@ -1,10 +1,17 @@
-import React from 'react';
-
-import './Chef.css';
+import React from "react";
+import { images } from "../../constants";
+import { SubHeading } from "../../components";
+import "./Chef.css";
 
 const Chef = () => (
-  <div>
-    Chef
+  <div className="app__bg app__wrapper section__padding">
+    <div className="app__wrapper_img app__wrapper_img-reverse">
+      <img src={images.chef} alt="chef_image" />
+    </div>
+
+    <div className="app__wrapper_info">
+      <SubHeading title="Chef’s Word" />
+    </div>
   </div>
 );
 
