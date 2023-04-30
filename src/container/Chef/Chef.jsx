7414a0eba91cb8ee3c42ce4d13d77c195c3f71a8
Nyakamba <omwegaenock@gmail.com>
2023-04-30 23:25:20 +0300

    chaf,jsx

--- src/container/Chef/Chef.jsx
+++ src/container/Chef/Chef.jsx
@@ -11,6 +11,18 @@ const Chef = () => (
 
     <div className="app__wrapper_info">
       <SubHeading title="Chef’s Word" />
+      <h1 className="headtext__cormorant">What we believe in</h1>
+      <div className="app__chef-content">
+        <div className="app__chef-content_quote">
+          <img src={image.quote} alt="quote" />
+          <p className="p__opensans">
+            auctor sit iaculis in arcu. Vulputate nulla lobortis mauris eget
+            sit. Nulla scelerisque scelerisque congue ac consequat, aliquam
+            molestie lectus eu. Congue iaculis integer curabitur semper sit
+            nunc.
+          </p>
+        </div>
+      </div>
     </div>
   </div>
 );
